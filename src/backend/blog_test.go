2d805835b3442bdcb0659a3c14f20caacd9ef2c3
Donald Vincenzo Danilo <thedonenzo@gmail.com>
2025-05-08 04:54:47 +1000

    added unit tests for BlogCreate backend function

--- src/backend/blog_test.go
+++ src/backend/blog_test.go
@@ -6,3 +6,60 @@ import (
 
 func TestPlaceholderBlog(t *testing.T) {
 }
+
+// BlogCreate Tests
+func TestSuccessfulBlogCreation(t *testing.T) {
+	data := data{users: []userInfo{}}
+	userId, error1 := UserRegister("Donald", "Danilo", "DoFn", "thedonenzo@gmail.com", "P1#rfcJMN1", &data);
+	if error1 != nil {
+		t.Errorf("Successful registration failed")
+	}
+
+	_, error2 := BlogCreate(userId, "Amazing", "A cool and interesting blog", &data)
+	if error2 != nil {
+		t.Errorf("Successful blog creation failed")
+	}
+}
+
+func TestFailedBlogCreationShortTitle(t *testing.T) {
+	data := data{users: []userInfo{}}
+	userId, error1 := UserRegister("Donald", "Danilo", "DoFn", "thedonenzo@gmail.com", "P1#rfcJMN1", &data);
+	if error1 != nil {
+		t.Errorf("Successful registration failed")
+	}
+
+	_, error2 := BlogCreate(userId, "Cool", "A cool and interesting blog", &data)
+	if error2 == nil {
+		t.Errorf("Blog creation with short title succeeded")
+	}
+}
+
+func TestFailedBlogCreationInvalidTitle(t *testing.T) {
+	data := data{users: []userInfo{}}
+	userId, error1 := UserRegister("Donald", "Danilo", "DoFn", "thedonenzo@gmail.com", "P1#rfcJMN1", &data);
+	if error1 != nil {
+		t.Errorf("Successful registration failed")
+	}
+
+	_, error2 := BlogCreate(userId, "Cool!!!!!!", "A cool and interesting blog", &data)
+	if error2 == nil {
+		t.Errorf("Blog creation with invalid title characters succeeded")
+	}
+}
+
+func TestFailedBlogCreationInvalidDescription(t *testing.T) {
+	data := data{users: []userInfo{}}
+	userId, error1 := UserRegister("Donald", "Danilo", "DoFn", "thedonenzo@gmail.com", "P1#rfcJMN1", &data);
+	if error1 != nil {
+		t.Errorf("Successful registration failed")
+	}
+
+	description := ""
+	for i := 0; i < 1001; i++ {
+		description = description + "a"
+	}
+	_, error2 := BlogCreate(userId, "Amazing", description, &data)
+	if error2 == nil {
+		t.Errorf("Blog creation with long description succeeded")
+	}
+}
