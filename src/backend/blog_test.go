7543318aea45c4ecf3b5f58049b39864f8c30c25
Donald Vincenzo Danilo <thedonenzo@gmail.com>
2025-05-09 05:00:08 +1000

    added unit tests for BlogList backend function

--- src/backend/blog_test.go
+++ src/backend/blog_test.go
@@ -60,3 +60,58 @@ func TestFailedBlogCreationInvalidDescription(t *testing.T) {
 		t.Errorf("Blog creation with long description succeeded")
 	}
 }
+
+// BlogList Tests
+func TestSuccessfulBlogList(t *testing.T) {
+	data := data{users: []userInfo{}}
+	userId, error1 := UserRegister("Donald", "Danilo", "DoFn", "thedonenzo@gmail.com", "P1#rfcJMN1", &data);
+	if error1 != nil {
+		t.Errorf("Successful registration failed")
+	}
+
+	_, error2 := BlogCreate(userId, "Amazing", "A cool and interesting blog", &data)
+	if error2 != nil {
+		t.Errorf("Successful blog creation failed")
+	}
+
+	_, error3 := BlogList(userId, "DoFn", &data);
+	if error3 != nil {
+		t.Errorf("Successful blog list failed")
+	}
+}
+
+func TestFailedBlogListUserId(t *testing.T) {
+	data := data{users: []userInfo{}}
+	userId, error1 := UserRegister("Donald", "Danilo", "DoFn", "thedonenzo@gmail.com", "P1#rfcJMN1", &data);
+	if error1 != nil {
+		t.Errorf("Successful registration failed")
+	}
+
+	_, error2 := BlogCreate(userId, "Amazing", "A cool and interesting blog", &data)
+	if error2 != nil {
+		t.Errorf("Successful blog creation failed")
+	}
+
+	_, error3 := BlogList(userId + "1", "DoFn", &data);
+	if error3 == nil {
+		t.Errorf("Blog list with invalid userId succeeded")
+	}
+}
+
+func TestFailedBlogListUsername(t *testing.T) {
+	data := data{users: []userInfo{}}
+	userId, error1 := UserRegister("Donald", "Danilo", "DoFn", "thedonenzo@gmail.com", "P1#rfcJMN1", &data);
+	if error1 != nil {
+		t.Errorf("Successful registration failed")
+	}
+
+	_, error2 := BlogCreate(userId, "Amazing", "A cool and interesting blog", &data)
+	if error2 != nil {
+		t.Errorf("Successful blog creation failed")
+	}
+
+	_, error3 := BlogList(userId, "DoFnow", &data);
+	if error3 == nil {
+		t.Errorf("Blog list with invalid username succeeded")
+	}
+}
